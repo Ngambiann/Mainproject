57744b269ae9fdbbd4cbc7e583738a4d24d8ddc4
ann-ngambi <ngambiann08@gmail.com>
2024-07-17 09:22:05 +0300

    icon&logo

---
+++ src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-app-bar colour="light-red-lighten-1">
+        <template>
+        <v-app-bar-nav-icon> </v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>Gems</v-app-bar-title>
+        <v-btn to="/">Homepage</v-btn>
+    </v-app-bar>
+</template>
